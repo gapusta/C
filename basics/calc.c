8cda73c606b139f388ea586a7def62581999f332
gapusta <gapusta97@gmail.com>
2024-09-25 20:50:48 +0600

    Initial commit

---
+++ basics/calc.c
@@ -0,0 +1,10 @@
+#include "calc.h"
+
+int add(int x, int y) {
+	return x + y;
+}
+
+int sub(int x, int y) {
+	return x - y;
+}
+
---
+++ basics/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "calc.h"
+
+int main(void) {
+	printf("%d\n", add(34, 35));
+	return 0;
+}
+
