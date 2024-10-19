9830524c4a53944dc055e03011b8c181bd59389c
gapusta <gapusta97@gmail.com>
2024-10-19 15:22:52 +0600

    minor fix - error handling

--- echo_server/simple_string_reader.c
+++ echo_server/simple_string_reader.c
@@ -7,33 +7,50 @@ enum rchk_ssr_state { // Archke simple string reader states
 	rchk_ssr_done
 };
 
-typedef enum rchk_ssr_state rchk_ssr_state;
-
 struct rchk_ssr { // Archke simple string reader 
-	rchk_ssr_state state;
+	enum rchk_ssr_state state;
 	int idx;
 	char* str;
 };
 
-typedef struct rchk_ssr rchk_ssr;
+// 0 - OK
+// 1 - Initialization failed
+// 200 - '+' was expected
+struct rchk_ssr_status {
+	int code;
+};
 
-rchk_ssr* rchk_ssr_new(int buffer_size) {
-	rchk_ssr* new = (rchk_ssr*) malloc(sizeof(rchk_ssr));
-	
+typedef enum rchk_ssr_state rchk_ssr_state;
+typedef struct rchk_ssr rchk_ssr;
+typedef struct rchk_ssr_status rchk_ssr_status;
+
+rchk_ssr* rchk_ssr_new(int buffer_size, rchk_ssr_status* status) {
+	rchk_ssr* new = (rchk_ssr*) malloc(sizeof(rchk_ssr) + (buffer_size + 1)); // '1' is for '\0'
+	if (new == NULL) {
+		status->code = 1;
+		return NULL;
+	}
+
 	new->state = rchk_ssr_start;
 	new->idx = 0;
-	new->str = (char*) malloc(buffer_size);
+	new->str = (char*) (new + sizeof(rchk_ssr));
+	status->code = 0;
 	
 	return new;	
 }
 
-void rchk_ssr_process(rchk_ssr* ssr, char* chunk, int occupied) {
+int rchk_ssr_process(rchk_ssr* ssr, char* chunk, int occupied, rchk_ssr_status* status) {
 	for (int idx=0; idx<occupied; idx++) {
 		char current = chunk[idx];
 
 		switch(ssr->state) {
 			case rchk_ssr_start:
-				if (current == '+') { ssr->state = rchk_ssr_read; } else { } // handle error "+ expected"
+				if (current == '+') { 
+					ssr->state = rchk_ssr_read; 
+				} else { 
+					status->code = 200;
+					return -1;		
+				}
 				
 				break;
 			case rchk_ssr_read:
@@ -52,28 +69,26 @@ void rchk_ssr_process(rchk_ssr* ssr, char* chunk, int occupied) {
 				break;
 		
 		}
-	}	
+	}
+
+	return 0;	
 }
 
-int rchk_sst_done(rchk_ssr* ssr) {
-	return ssr->state == rchk_ssr_done; 
-}
+int rchk_sst_done(rchk_ssr* ssr) { return ssr->state == rchk_ssr_done; }
 
-char* rchk_ssr_str(rchk_ssr* ssr) {
-	return ssr->str;
-}
+char* rchk_ssr_str(rchk_ssr* ssr) { return ssr->str; }
 
-void rchk_ssr_free(rchk_ssr* ssr) {
-	free(ssr->str);
-	free(ssr);
-}
+int rchk_ssr_free(rchk_ssr* ssr) { free(ssr); }
 
 int main(void) {
-	rchk_ssr* reader = rchk_ssr_new(256);
+	rchk_ssr_status status;
+	status.code = 0;
 
-	char* input = "+heyyyyyy\r\n";
+	rchk_ssr* reader = rchk_ssr_new(3, &status);
 
-	rchk_ssr_process(reader, input, 11);
+	char* input = "+hey\r\n";
+
+	rchk_ssr_process(reader, input, 6, &status);
 
 	printf("%s\n", rchk_ssr_str(reader));
 
