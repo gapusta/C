5ce66f28c0da065600df4b2291f6d7683dd1a48a
gapusta <gapusta97@gmail.com>
2024-11-21 12:52:07 +0600

    up

--- echo_server/archke_event_handlers.c
+++ echo_server/archke_event_handlers.c
@@ -18,57 +18,57 @@ void error(const char *msg) {
 void rchkHandleWriteEvent(RchkEventLoop* eventLoop, int fd, struct RchkEvent* event, void* clientData) {
 	int chunk_size = 256;
 	char chunk[chunk_size];
-	Client* c = (Client*) clientData;
-	char* payload = rchkStringReaderData(c->reader);
-	int str_size = rchkStringReaderDataSize(c->reader);
+	Client* client = (Client*) clientData;
+	char* payload = rchkStringReaderData(client->reader);
+	int str_size = rchkStringReaderDataSize(client->reader);
 	int occupied = 0;
 	int prefix_size = 0;
 	int payload_size = 0;
 	int suffix_size = 0;
 
 	// 1. compute and set message prefix if needed
-	if (c->sent < 1) {
+	if (client->sent < 1) {
 		chunk[occupied++] = '+';
 		prefix_size = 1;
 	}
 
 	// 2. copy message part to buffer
-	int remaining = str_size - c->sent;
+	int remaining = str_size - client->sent;
 	int chunk_available = chunk_size - prefix_size;
 
 	payload_size = min(remaining, chunk_available);
 
 	for (int pidx=0; pidx < payload_size; pidx++, occupied++) {
-		chunk[occupied] = payload[c->sent + pidx];
+		chunk[occupied] = payload[client->sent + pidx];
 	}
 
 	// 3. deal with '\r'
-	if (c->sent < str_size + 2 && occupied < chunk_size) {
+	if (client->sent < str_size + 2 && occupied < chunk_size) {
 		chunk[occupied++] = '\r';
 		suffix_size++;
 	}
 
 	// 4. deal with '\n'
-	if (c->sent < str_size + 3 && occupied < chunk_size) {
+	if (client->sent < str_size + 3 && occupied < chunk_size) {
 		chunk[occupied++] = '\n';
 		suffix_size++;
 	}
 
 	// send data
-	int nbytes = rchkSocketWrite(c->fd, chunk, prefix_size + payload_size + suffix_size);
+	int nbytes = rchkSocketWrite(client->fd, chunk, prefix_size + payload_size + suffix_size);
 	if (nbytes < 0) {
 		error("read() error");
 		return;
 	}
 
-	c->sent = c->sent + nbytes;
+	client->sent = client->sent + nbytes;
 	
-	if (c->sent == str_size + 3) {
+	if (client->sent == str_size + 3) {
 		// all the data has been sent
-		c->sent = 0;
-		rchkStringReaderClear(c->reader);
+		client->sent = 0;
+		rchkStringReaderClear(client->reader);
 		// register read handler for new client
-		int result = rchkEventLoopRegister(eventLoop, c->fd, ARCHKE_EVENT_LOOP_READ_EVENT, rchkHandleReadEvent, c);
+		int result = rchkEventLoopRegister(eventLoop, client->fd, ARCHKE_EVENT_LOOP_READ_EVENT, rchkHandleReadEvent, client);
 		if (result == -1) {
 			error("client socket read event registration error");
 		}
@@ -77,27 +77,27 @@ void rchkHandleWriteEvent(RchkEventLoop* eventLoop, int fd, struct RchkEvent* ev
 
 void rchkHandleReadEvent(RchkEventLoop* eventLoop, int fd, struct RchkEvent* event, void* clientData) {
 	char chunk[256];
-	Client* c = (Client*) clientData;
+	Client* client = (Client*) clientData;
 	
-	int nbytes = rchkSocketRead(c->fd, chunk, sizeof(chunk));
+	int nbytes = rchkSocketRead(client->fd, chunk, sizeof(chunk));
 
 	if (nbytes == -1) {
 		error("read() error");
 	}
 
 	if (nbytes == 0) {
-		rchkEventLoopUnregister(eventLoop, c->fd);
-		rchkSocketClose(c->fd);
-		rchkStringReaderFree(c->reader);
-		free(c);
+		rchkEventLoopUnregister(eventLoop, client->fd);
+		rchkSocketClose(client->fd);
+		rchkStringReaderFree(client->reader);
+		free(client);
 		return;
 	}
 
-	rchkStringReaderProcess(c->reader, chunk, nbytes);
+	rchkStringReaderProcess(client->reader, chunk, nbytes);
 
-	if (rchkStringReaderIsDone(c->reader)) {
+	if (rchkStringReaderIsDone(client->reader)) {
 		// register write handler for client
-		int result = rchkEventLoopRegister(eventLoop, c->fd, ARCHKE_EVENT_LOOP_WRITE_EVENT, rchkHandleWriteEvent, c);
+		int result = rchkEventLoopRegister(eventLoop, client->fd, ARCHKE_EVENT_LOOP_WRITE_EVENT, rchkHandleWriteEvent, client);
 		if (result == -1) {
 			error("client socket write event registration error");
 		}
@@ -113,13 +113,13 @@ void rchkHandleAcceptEvent(RchkEventLoop* eventLoop, int fd, struct RchkEvent* e
 	RchkStringReader* reader = rchkStringReaderNew(1024);
 
 	// initialize client data
-	Client* c = malloc(sizeof(Client));
-	c->fd = clientSocketFd;
-	c->reader = reader;
-	c->sent = 0;
+	Client* client = malloc(sizeof(Client));
+	client->fd = clientSocketFd;
+	client->reader = reader;
+	client->sent = 0;
 
 	// register read handler for new client
-	int result = rchkEventLoopRegister(eventLoop, clientSocketFd, ARCHKE_EVENT_LOOP_READ_EVENT, rchkHandleReadEvent, c);
+	int result = rchkEventLoopRegister(eventLoop, clientSocketFd, ARCHKE_EVENT_LOOP_READ_EVENT, rchkHandleReadEvent, client);
 	if (result == -1) {
 		error("client socket read event registration error");
 	}
